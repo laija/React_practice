152c00cba40017ab20a78871a78f66d0342f8cc3
luis manuel laija rojas <laija11@hotmail.com>
2022-04-08 13:00:58 -0600

    layout created in css grid

---
+++ general_layout/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <nav>Navbar</nav>
+      <main>Main</main>
+      <div id="sidebar">Sidebar</div>
+      <div id="content1">Constent1</div>
+      <div id="content2">Content2</div>
+      <div id="content3">Content3</div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
